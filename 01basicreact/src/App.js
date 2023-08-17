c67415ffc13258fed24adc8db365ca995ca8fcfc
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2023-08-17 23:40:31 +0530

    created 2 react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>chai aur react | </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+   <h1>Chai aur react with vite | HC</h1>
+  )
+}
+
+export default App
