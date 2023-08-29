1fa9d3c42f820f9345abe093018f7b63fc28c182
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-08-29 23:08:38 +0500

    Added My Infromation Sir

--- 01basicreact/src/Chai.jsx
+++ 01basicreact/src/Chai.jsx
@@ -1,7 +1,21 @@
-function Chai(){
-    return(
-        <h2>chai in react</h2>
-    )
+function Chai() {
+  return (
+    <tr class="hover:bg-gray-200">
+      <td class="border px-4 py-2">Jeet </td>
+      <td class="border px-4 py-2">Hyderabad</td>
+      <td class="border px-4 py-2">Pakistan</td>
+      <td class="border px-4 py-2">muhammadmohsinsiddique789@gmail.com</td>
+      <td class="border px-4 py-2">
+        <a
+          href="https://github.com/muhammad-mohsin-siddique"
+          class="text-blue-500"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </td>
+    </tr>
+  );
 }
 
-export default Chai
+export default Chai;
