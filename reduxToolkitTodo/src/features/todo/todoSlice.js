63eaf483b952e6913b128c79ae748ce167f79473
Anas Siddiqui <111227461+AnasSiddiqui18@users.noreply.github.com>
2023-09-17 04:15:07 +0500

    Update todoSlice.js

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,28 +1,39 @@
-import {createSlice, nanoid } from '@reduxjs/toolkit';
+// todoSlice.js
+
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1, text: "Hello world"}]
-}
-
-
+  todos: [{ id: nanoid(), text: "Hello world" }],
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
-                id: nanoid(), 
-                text: action.payload
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
-        },
-    }
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
 
-export const {addTodo, removeTodo} = todoSlice.actions
+      //? Added update todo functionality so the user can also update the todos.
+    updateTodo: (state, action) => {
+      const updatedTodo = action.payload;
+      const todoToUpdate = state.todos.find(
+        (todo) => todo.id === updatedTodo.id
+      );
+      if (todoToUpdate) {
+        todoToUpdate.text = updatedTodo.text;
+      }
+    },
+  },
+});
 
-export default todoSlice.reducer
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
