66e14d80c6bf7338769e2f089985d9cabbbd4361
Emperor329 <95495259+Emperor329@users.noreply.github.com>
2023-09-30 00:11:46 +0530

    Added App component in main.jsx

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -79,8 +79,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>,
+    <Provider store={store} >
+      <App />
+    </Provider >
+  </React.StrictMode>
+  
 )
+
