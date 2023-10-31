02a7ba0e9c995a298c88bb340c3d95de392a0489
Ganeswar Sahoo <naganeswar787254@gmail.com>
2023-10-31 23:46:33 +0530

    Fixed a bug in the swap function logic

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -15,11 +15,19 @@ function App() {
   const options = Object.keys(currencyInfo)
 
   const swap = () => {
-    setFrom(to)
-    setTo(from)
-    setConvertedAmount(amount)
-    setAmount(convertedAmount)
-  }
+    // Save the current 'from' and 'to' values
+    const currentFrom = from;
+    const currentTo = to;
+  
+    // Swap 'from' and 'to'
+    setFrom(currentTo);
+    setTo(currentFrom);
+  
+    // Swap 'amount' and 'convertedAmount'
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  };
+  
   
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
