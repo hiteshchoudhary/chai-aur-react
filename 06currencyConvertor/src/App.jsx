73ecf135402547e693b0f7fa19dfe91eb14f848f
Ashish Vishwakarma <ashishvishwakarma7519@gmail.com>
2023-12-10 12:53:43 +0530

    fix currency updation

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -25,8 +25,6 @@ function App() {
     setConvertedAmount(amount * currencyInfo[to])
   }
 
-  // TODO: work on issue #34
-
   return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
