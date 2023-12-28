cb17a294a0ed968ea12417af2d34087721694194
krishnagitcontri <gitrepokrishna@gmail.com>
2023-12-29 01:30:57 +0530

    fasf

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -20,9 +20,11 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className="hover:underline">
+                                    <Link to="/" 
+                                    
+                                    className="hover:underline">
                                         Home
-                                    </Link>
+                                    </Link> 
                                 </li>
                                 <li>
                                     <Link to="/about" className="hover:underline">
