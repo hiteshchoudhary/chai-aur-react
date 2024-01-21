b911437f4c547f4e27e609d426e341e664461c20
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2024-01-21 23:03:19 +0530

    Updated onCurrencyChange in "From" inputBox in App.jsx
    It was a small bug, i know you have leaved it intentionally : )
    
    thank you sir : )

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
                             label="From"
                             amount={amount}
                             currencyOptions={options}
-                            onCurrencyChange={(currency) => setAmount(amount)}
+                            onCurrencyChange={(currency) => setFrom(currency)}
                             selectCurrency={from}
                             onAmountChange={(amount) => setAmount(amount)}
                         />
